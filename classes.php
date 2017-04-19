bdf42721ee8e29df0de2e7dd4fb3c41ac7d21732
Piotr Pesta <piotr.pesta@gmail.com>
2017-04-19 17:26:59 +0200

    v0.2.0
    +settings page
    +orders randomizer
    *code tweaks

--- classes.php
+++ classes.php
@@ -27,21 +27,24 @@ class Someone_Recently_Bought_Init { # Initialization
     static function activate_plugin() {
         add_option('recently_bought_settings_pnumber', 5);
         add_option('recently_bought_settings_text', 'recently bought');
+        add_option('recently_bought_settings_randomize', 0);
     }
 
     public static function uninstall_plugin() {
         delete_option('recently_bought_settings_pnumber');
         delete_option('recently_bought_settings_text');
+        delete_option('recently_bought_settings_randomize');
     }
 
     public static function admin_init() {
-        
+        register_setting('recently_bought_settings_group', 'recently_bought_settings_text');
+        register_setting('recently_bought_settings_group', 'recently_bought_settings_pnumber');
+        register_setting('recently_bought_settings_group', 'recently_bought_settings_randomize');
     }
 
     public static function admin_menu() {
-        require_once( RECENTLY_BOUGHT_PLUGIN_DIR . 'views/options.php' );
         $optionsTitle = __('Recently Bought', 'recently-bought');
-        add_menu_page($optionsTitle, $optionsTitle, 'administrator', 'rbset', 'recently_bought_settings', 'dashicons-carrot');
+        add_menu_page($optionsTitle, $optionsTitle, 'administrator', 'recently-bought-settings', array('Someone_Recently_Bought_Init', 'options_page'), 'dashicons-carrot');
     }
 
     public static function just_init() {
@@ -52,84 +55,88 @@ class Someone_Recently_Bought_Init { # Initialization
         wp_enqueue_style('pp_recently_bought_for_woocommerce_main_style', plugins_url('_inc/recently-bought-style.css', __FILE__));
     }
 
+    public static function options_page() {
+        require_once( RECENTLY_BOUGHT_PLUGIN_DIR . 'views/options.php' );
+    }
+
 }
 
 class Someone_Recently_Bought_Main {
 
     public static function main_draw() {
-        if (current_user_can('administrator')) {
-            $toShow = self::data_miner();
-            ?>
+        //if (current_user_can('administrator')) {
+        $toShow = self::data_miner();
+        ?>
 
-            <script type="text/javascript">
-                jQuery(document).ready((function () {
-                    function getCookie(cname) {
-                        var name = cname + "=";
-                        var decodedCookie = decodeURIComponent(document.cookie);
-                        var ca = decodedCookie.split(';');
-                        for (var i = 0; i < ca.length; i++) {
-                            var c = ca[i];
-                            while (c.charAt(0) == ' ') {
-                                c = c.substring(1);
-                            }
-                            if (c.indexOf(name) == 0) {
-                                return c.substring(name.length, c.length);
-                            }
+        <script type="text/javascript">
+            jQuery(document).ready((function () {
+                function getCookie(cname) {
+                    var name = cname + "=";
+                    var decodedCookie = decodeURIComponent(document.cookie);
+                    var ca = decodedCookie.split(';');
+                    for (var i = 0; i < ca.length; i++) {
+                        var c = ca[i];
+                        while (c.charAt(0) == ' ') {
+                            c = c.substring(1);
                         }
-                        return "";
+                        if (c.indexOf(name) == 0) {
+                            return c.substring(name.length, c.length);
+                        }
                     }
+                    return "";
+                }
 
-                    var cCookie = getCookie("justBought");
+                var cCookie = getCookie("justBought");
 
-                    var toShow = <?php echo $toShow; ?>;
+                var toShow = <?php echo $toShow; ?>;
 
-                    if (cCookie == "" && typeof toShow !== 'undefined' && toShow.length > 0) {
-                        jQuery('#justBought').dialog({
-                            position: {my: 'right bottom', at: 'right bottom', of: window},
-                            dialogClass: 'fixed-dialog',
-                            draggable: false,
-                            resizable: false,
-                            show: {effect: 'fade', duration: 1000},
-                            hide: {effect: 'fade', duration: 1000},
-                            close: function (event, ui) {
-                                var date = new Date();
-                                date.setTime(date.getTime() + (600 * 1000));
-                                var expires = "; expires=" + date.toGMTString();
-                                document.cookie = "justBought = closed;" + expires + "; path=/";
-                            }
-                        });
+                if (cCookie == "" && typeof toShow !== 'undefined' && toShow.length > 0) {
+                    jQuery('#justBought').dialog({
+                        position: {my: 'right bottom', at: 'right bottom', of: window},
+                        dialogClass: 'fixed-dialog',
+                        draggable: false,
+                        resizable: false,
+                        show: {effect: 'fade', duration: 1000},
+                        hide: {effect: 'fade', duration: 1000},
+                        close: function (event, ui) {
+                            var date = new Date();
+                            date.setTime(date.getTime() + (600 * 1000));
+                            var expires = "; expires=" + date.toGMTString();
+                            document.cookie = "justBought = closed;" + expires + "; path=/";
+                        }
+                    });
 
-                        jQuery('#itemsToShow').html(toShow[0]);
+                    jQuery('#itemsToShow').html(toShow[0]);
 
-                        var timesRun = 0;
-                        var numberOfOrders = 4;
-                        var interval = setInterval(function () {
-                            timesRun += 1;
-                            if (timesRun > numberOfOrders) {
-                                jQuery('#justBought').dialog('destroy');
-                                jQuery('p').remove('#itemsToShow');
-                                var date = new Date();
-                                date.setTime(date.getTime() + (15 * 1000));
-                                var expires = "; expires=" + date.toGMTString();
-                                document.cookie = "justBought = closed;" + expires + "; path=/";
-                                clearInterval(interval);
-                            } else if (timesRun <= numberOfOrders) {
-                                jQuery('#itemsToShow').fadeOut(500, function () {
-                                    jQuery(this).html(toShow[timesRun]).fadeIn(500);
-                                });
-                            }
-                        }, 5 * 1000);
-                    }
+                    var timesRun = 0;
+                    var numberOfOrders = <?php echo get_option('recently_bought_settings_pnumber') ?> - 1;
+                    var interval = setInterval(function () {
+                        timesRun += 1;
+                        if (timesRun > numberOfOrders) {
+                            jQuery('#justBought').dialog('destroy');
+                            jQuery('p').remove('#itemsToShow');
+                            var date = new Date();
+                            date.setTime(date.getTime() + (15 * 1000));
+                            var expires = "; expires=" + date.toGMTString();
+                            document.cookie = "justBought = closed;" + expires + "; path=/";
+                            clearInterval(interval);
+                        } else if (timesRun <= numberOfOrders) {
+                            jQuery('#itemsToShow').fadeOut(500, function () {
+                                jQuery(this).html(toShow[timesRun]).fadeIn(500);
+                            });
+                        }
+                    }, 5 * 1000);
+                }
 
 
-                }));
-            </script>
-            <div id="justBought" title="">
-                <p id="itemsToShow"></p>
-            </div>
+            }));
+        </script>
+        <div id="justBought" title="">
+            <p id="itemsToShow"></p>
+        </div>
 
-            <?php
-        }
+        <?php
+        //}
     }
 
     public static function data_miner() {
@@ -141,11 +148,20 @@ class Someone_Recently_Bought_Main {
             $orders[$c] = new WC_Order($ordersToShow[$c]->ID);
             $items[$c] = $orders[$c]->get_items();
             $items[$c] = array_values($items[$c]);
-            $htmlToShow[$c] = '<a href="' . get_permalink($items[$c][0]['product_id']) . '">' . get_the_post_thumbnail($items[$c][0]['product_id'], 'thumbnail', array('style' => 'height:80px;width:auto;', 'class' => 'alignleft')) . $orders[$c]->shipping_first_name . ' ' . get_option('recently_bought_settings_text') . ' </br><span id="productTitle">' . $items[$c][0]['name'] . '</span></a>';
+            $htmlToShow[$c] = '<a href="' .
+                    get_permalink($items[$c][0]['product_id']) .
+                    '">' .
+                    get_the_post_thumbnail($items[$c][0]['product_id'], 'shop_thumbnail', array('style' => 'height:80px;width:auto;', 'class' => 'alignleft')) .
+                    $orders[$c]->shipping_first_name . ' ' .
+                    get_option('recently_bought_settings_text') .
+                    ' </br><span id="productTitle">' . $items[$c][0]['name'] .
+                    '</span></a>';
         }
 
         if (isset($htmlToShow)) {
-            var_dump(get_option('recently_bought_settings_text')); //dlaczego nie dodaje opcji, wyjaśnić
+            if (get_option('recently_bought_settings_randomize') == 1) {
+                shuffle($htmlToShow);
+            }
             $toShow = json_encode($htmlToShow);
         } else {
             $toShow = '';
